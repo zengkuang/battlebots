d0335bb2dc0178305e25c9ee18d7cb02d4f9b4f0
Beck <pshfls@gmail.com>
2017-10-02 19:28:54 +0800

    NEED FIX: workaround for HAL and system library mismatch

--- Src/system_stm32f4xx.c
+++ Src/system_stm32f4xx.c
@@ -138,6 +138,9 @@
   uint32_t SystemCoreClock = 16000000;
 const uint8_t AHBPrescTable[16] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 6, 7, 8, 9};
 
+/* Beck: Only a workaround for HAL library and standard library mismatch */
+const uint8_t APBPrescTable[8] = {0, 0, 0, 0, 1, 2, 3, 4};
+
 /**
   * @}
   */
